b7bbc7d84d66b38a59f534f005115600ff5fdf8f
David Yin <yin.dydx@gmail.com>
2017-03-27 22:01:34 -0700

    did math 307

--- MATH 307/ass6/q5.m
+++ MATH 307/ass6/q5.m
@@ -5,10 +5,13 @@ load matrixforq5.mat;
 
 x = ones(298,1);
 
-for i = 1:30
-    x = A*x/norm(A*x);
+B = A;
+
+for i = 1:10
+    y = B\x;
+    x = y/norm(y);
 end
 
-eig1 = (x'*A*x)/(x'*x);
+eig1 = (x'*B*x)/(x'*x);
 
-% The estimate for the eigenvalue is -3.5342*10^4
+% The estimate for the eigenvalue is -1.
